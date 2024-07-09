2490806acbadac488ba659aaaa36c2e5391f21ac
hyein <165127040+yu-hazel@users.noreply.github.com>
2024-07-09 11:58:15 +0900

    Delete assets directory

--- assets/ReadingStatistics-BX4HhoDl.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,a as t,b as s,e as a}from"./index-nc3RoHgT.js";const n={name:"ReadingStatistics"},o=a("h1",null,"나의 독서통계",-1),c=[o];function i(r,_,d,l,p,f){return t(),s("div",null,c)}const m=e(n,[["render",i]]);export{m as default};
