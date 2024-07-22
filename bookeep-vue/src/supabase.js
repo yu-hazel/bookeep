274d2b548690f983a07c882a81ceb5fd9f03524a
hyein <yoohi960201@gmail.com>
2024-07-22 11:57:48 +0900

    빌드 파일, 원본 파일 삭제

--- bookeep-vue/src/supabase.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://vovzpunshmcodmkletaw.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvdnpwdW5zaG1jb2Rta2xldGF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgxMDg5MzUsImV4cCI6MjAzMzY4NDkzNX0.aVXi_AbLx-AGNwWVa0MB-WiNrkht2Eq0ECnBwDds2Kc';
-export const supabase = createClient(supabaseUrl, supabaseKey);
